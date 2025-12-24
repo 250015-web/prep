d478572f1d79ec9d2e179c5f0559afe57d9e8cda
250015-web <250015@newuu.uz>
2025-12-25 03:13:24 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,171 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main () {
+    // int (n);
+    // cin >> n;
+    // cout << 32 << endl;
+    // int x, y;
+    // cin >> x;
+    // cin >> y;
+    // cout << (x+y) << endl;
+    // int x,y,z;
+    // cin>>x>>y>>z;
+    // cout<<(x*y*z)/z<<endl;
+    // int x, P;
+    // cin >>x;
+    // P = 4*x;
+    // cout<<P;
+    // float d, pi=3.14, L;
+    // cin >> d;
+    // L=d*pi;
+    // cout << L << endl;
+    // float x, y, A;
+    // cout << "Enter two numbers: ";
+    // cin >> x >> y;
+    // A=(x+y)/2;
+    // cout << "The mean is " << A << endl;
+    // int side, V, S;
+    // cout << "Enter side side: ";
+    // cin >> side;
+    // V= pow(side, 3);
+    // cout <<"The volume is:"<< V<< endl;
+    // S= 6*pow(side, 2);
+    // cout <<"The sum is: "<< S<< endl;
+    // float x, SQRT;
+    // cin >> x;
+    // SQRT = sqrt(x);
+    // cout << SQRT;
+    // int x, absolute;
+    // cin >> x;
+    // absolute = abs(x);
+    // cout << absolute << endl;
+    // float x,y,sum, product, ma, mb  ;
+    // cout<< "enter x and y: ";
+    // cin>>x>>y;
+    // sum=x+y;
+    // cout<< "The sum of x and y is: " << sum << endl;
+    // product=x*y;
+    // cout<< "the product of x and y is : "<<product<< endl;
+    // ma=abs(x);
+    // cout<< "The absolute value of x and y is: " << ma<< endl;
+    // mb=abs(y);
+    // cout<< "The absolute value of x and y is: "<< mb<< endl;
+    // int x, y, tseloye, netseloye;
+    // cout << "enter two numbers: " << endl;
+    // cin >> x >> y;
+    // tseloye = x/y;
+    // cout << " the real number " << tseloye << endl;
+    // netseloye = x%y;
+    // cout<< "netseloye = " << netseloye << endl;
+    // int uch, yuz, bir;
+    // cin >> uch;
+    // yuz = uch/100;
+    // bir = uch%10;
+    // bool k = yuz == bir;
+    // cout << boolalpha<< k << endl;
+    // float x, y, z, D;
+    // cout<< "Enter your integers:" << x<<y<<z<<endl;
+    // cin >> x >> y >> z;
+    // D = pow(y,2) - 4*x*z;
+    // bool k = D>=0;
+    // cout << boolalpha<<k << endl;
+    // int uch, yuz, bir;
+    // cout << "Uch xonali son kiriting:";
+    // cin >> uch;
+    // yuz = uch/100;
+    // bir = uch%10;
+    // if (yuz == bir) {
+    //     cout << "Palindrome";
+    // } else {
+    //     cout << "Not Palindrome";
+    // }
+    // float a, b , c, D;
+    // cout << "Enter the numbers" << endl;
+    // cin >> a >> b >> c;
+    // D= pow (b, 2) - 4*a*c;
+    // if (D >= 0) {
+    //     cout<<"There's at least one solution "<<endl;
+    // } else {
+    //     cout<<"There's no solution "<<endl;
+    // }
+    // int x;
+    // cin >> x;
+    // if (x > 0) {
+    //     x++;
+    // } else {
+    //     x == x;
+    // }
+    // cout << x << endl;
+    // int x;
+    // cin >> x;
+    // if (x>0) {
+    //     x++;
+    // } else {
+    //     x=x-2;
+    // }
+    // cout << x << endl;
+    // int x;
+    // cin>>x;
+    // if (x>0) {
+    //     cout<<"Positive"<<endl;
+    // } else if (x<0) {
+    //     cout<<"Negative"<<endl;
+    // } else {
+    //     cout<<"x=Zero"<<endl;
+    // }
+    // cout<<x<<endl;
+    // int x, y , max;
+    // cin >> x >> y;
+    // if (x > y) {
+    //     max = x; cout << "max is:" << max << endl;
+    // } else if (y > x) {
+    //     max = y; cout << "max is:" << max << endl;
+    // } else {
+    //     cout << "They are eqaul" << endl;
+    // }
+    // int x, y, z, max;
+    // cin >> x >> y >> z;
+    // if (x > y && x > z) {
+    //     max = x;
+    // } else if (y > x && y > z) {
+    //     max = y;
+    // } else {
+    //     max = z;
+    // }
+    // cout << max << endl;
+//     int x , y, z, counter=0;
+//     cin >> x >> y >> z;
+//      if (x >=0) {
+//         counter++;
+//     } if (y  >=0) {
+//         counter++;
+//     } if (z >=0) {
+//         counter++;
+//     }
+// cout << counter << endl;
+    // int x;
+    // cout << "Enter a day's num: ";
+    // cin >> x;
+    // switch (x) {
+    //     case 1: cout << " It is Monday" << endl; break;
+    //         case 2: cout << " It is Tuesday" << endl; break;
+    //         case 3: cout << " It is Wednesday" << endl; break;
+    //         case 4: cout << " It is Thursday" << endl; break;
+    //         case 5: cout << " It is Friday" << endl; break;
+    //         case 6: cout << " It is Saturday" << endl; break;
+    //         case 7: cout << " It is Sunday" << endl; break;
+    //         default: cout << "error" << endl;
+    // }
+    // int score;
+    // cin >> score;
+    // switch (score) {
+    //     case 5: cout << "Excellent!" << endl; break;
+    //         case 4: cout << "Good!" << endl; break;
+    //         case 3: cout << "Not bad!" << endl; break;
+    //         case 2: cout << "Bad!" << endl; break;
+    //         case 1: cout << "Awful!" << endl; break;
+//             default: cout << "Bro What!?" << endl; break;
+//     }
+    return 0;
+}
